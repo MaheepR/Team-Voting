933cbf806b17db726878a5af677637be1adffd88
MaheepR <85050293+MaheepR@users.noreply.github.com>
2021-11-24 18:45:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+apiKey: "AIzaSyBhfVfpV97gOfdxUSiutjbNBBO9NSTt2ao",
+  authDomain: "class-app-f6df3.firebaseapp.com",
+  databaseURL: "https://class-app-f6df3-default-rtdb.firebaseio.com",
+  projectId: "class-app-f6df3",
+  storageBucket: "class-app-f6df3.appspot.com",
+  messagingSenderId: "768015098559",
+  appId: "1:768015098559:web:5983d3542c6c7a5fe85c0a"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
